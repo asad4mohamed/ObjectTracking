e1af53d2e35623e42e2041597ae552d19f0cd374
asad4mohamed <asad4mohamed@gmail.com>
2024-08-11 21:37:28 -0500

    Add files via upload

---
+++ Cartracking.py
@@ -0,0 +1,48 @@
+import cv2
+screen = cv2.VideoCapture("bb3.mp4")
+
+#loadTracker
+tracker = cv2.TrackerCSRT_create()
+
+#capture the first frame
+ret, frame = screen.read()
+
+#select the bounding box
+bbox = cv2.selectROI("tracking", frame, False)
+
+#initialize tracker
+ok = tracker.init(frame, bbox)
+print(bbox)
+
+#draw boundBox
+def drawbox(frame,bbox):
+    #get the value of bbox
+    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+
+    #draw the rectangle using cv2
+    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3,1)
+
+    #Write a text or a frame
+    cv2.putText(frame,"Tracking",(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,0,0),2)
+while True:
+    #read a new frame
+    ret, frame = screen.read()
+
+    #update tracker
+    ok, bbox = tracker.update(frame)
+
+    if ok:
+        #draw the bounding box
+        drawbox(frame, bbox)
+
+    else:
+        #if the tracker is not found, print a message
+        cv2.putText(frame,"Tracking failed",(10,20),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,0,0),2)
+
+    #display the frame
+    cv2.imshow("tracking", frame)
+
+
+
+
+
